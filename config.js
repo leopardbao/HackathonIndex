6db92c808fe17fc132a7ee90147cb5ca1d634516
Jie Bao <baojie0217@gmail.com>
2017-09-20 23:47:02 -0500

    Renamed config file

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    time: "Nov 5, 2017 08:00:00",
+    title: "My<br>Hackathon",
+    qa: [{question:"What should I bring",answer:"Lorem ipsum dolor sit amet, feugiat ut nibh. Eros dignissim est sapien, donec"},{question:"Is there shower for the night", answer:"Lorem ipsum dolor sit amet, feugiat ut nibh. Eros dignissim est sapien, donec"}]
+}
